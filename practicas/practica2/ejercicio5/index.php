2b970d13d58378aed24a8e2b70f3d7adc5c61f7b
Ramon253 <jgalazc253@g.educaand.es>
2023-11-08 12:44:52 +0100

    ejercicio9.php y ejercicio5.php empezados

--- practicas/practica2/ejercicio5/index.php
+++ practicas/practica2/ejercicio5/index.php
@@ -9,7 +9,12 @@
 </head>
 <body>
 <form action="ejercicio5.php" method="post">
-    
+    <label for="race">Raza1
+        <input type="radio" name="race" id="race1">
+    </label>
+    <label for="race2">Raza 2
+        <input type="radio" name="race" id="race2">
+    </label>
 </form>
 </body>
 </html>
--- practicas/practica2/ejercicio9/ejercicio9.php
+++ practicas/practica2/ejercicio9/ejercicio9.php
@@ -1,2 +1,28 @@
 <?php
-require "./views/head" . $_POST["header"] . ".html";
+$header = $_POST["header"];
+$body = $_POST["body"];
+$footer = $_POST["footer"];
+$css = $_POST["css"];
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio 9</title>
+    <link rel="stylesheet" href="./css/style<?= $css ?>.css">
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+//TODO
+require 'views/header/header'.$header.'.php';
+require 'views/body/body'.$body.'.php';
+require 'views/footer/footer'.$footer.'.php';
+?>
