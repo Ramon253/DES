b6ef080a621ff9d19ddb14fe56e0cc39c103cee5
Ramon253 <jgalazc253@g.educaand.es>
2023-11-08 12:41:48 +0100

    ejercicio8.php terminado

--- practicas/practica2/ejercicio8/ejercicio8.php
+++ practicas/practica2/ejercicio8/ejercicio8.php
@@ -1,8 +1,10 @@
 <?php
+const SAMPLE = ['J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14];
 $hand = [$_POST["Primera"], $_POST["Segunda"], $_POST["Tercera"], $_POST["Cuarta"], $_POST["Quinta"]];
 $suits = [];
 $displayValues = [];
 $numericValues = [];
+
 
 $color = false;
 $full = false;
@@ -24,27 +26,42 @@ foreach ($hand as $card) {
         $suits[] = substr($card, 2);
         continue;
     }
+
+    $suits[] = substr($card, 1);
     if (!is_numeric($card[0])){
-        switch ($card[0]){
-            case 'J': 
-        }
+        $numericValues[] = SAMPLE[$card[0]];
+        $displayValues[] = $card[0];
+        continue;
     }
 
     $displayValues[] = $card[0];
-    $suits[] = substr($card, 1);
+    $numericValues[] = intval($card[0]);
 }
 
-$sortedNumbers = $displayValues;
+$sortedNumbers = $numericValues;
 sort($sortedNumbers);
 
+//check if is color
 if (count(array_unique($suits)) == 1) {
     $color = true;
 }
 
+//check if is full or poker
 if(count(array_unique($displayValues))== 2){
     if ($sortedNumbers[1]  !== $sortedNumbers[3]) $full = true;
     else $poker = true;
 }
+
+//check if is straigt
+for ($i = 0; $i < count($sortedNumbers)-1;) {
+    if ($sortedNumbers[$i] === ($sortedNumbers[++$i]) - 1) {
+        $straight = true;
+        continue;
+    }
+    $straight = false;
+    break;
+}
+
 ?>
 
 <!doctype html>
@@ -59,11 +76,22 @@ if(count(array_unique($displayValues))== 2){
 <body>
 <h1>Tus cartas:</h1>
 <?php
-if ($color)
-    echo "<h1>TIENES COLOR</h1>";
+
+if ($color) {
+    if ($straight)
+        echo "<h1>ESCALERA DE COLOR</h1>";
+    else
+        echo "<h1>TIENES COLOR</h1>";
+
+} elseif ($straight) {
+    echo '<h1>TIENES ESCALERA</h1>';
+}
+
 if ($full)
     echo "<h1>TIENES UN FULL</h1>"
+
 ?>
+
 <div style="display: flex">
     <?php
 
