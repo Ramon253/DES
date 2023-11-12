0e52d795ccf5c695fc9ec6bbb9a44a39aed57549
Ramon253 <jgalazc253@g.educaand.es>
2023-11-12 18:25:58 +0100

    ejercicio9 en proceso

--- practicas/practica2/ejercicio9/views/body/body2.php
+++ practicas/practica2/ejercicio9/views/body/body2.php
@@ -1,3 +1,9 @@
-<main>
-
+<main class="cs2">
+    <section>
+        <h2>Gameplay</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at expedita ipsa totam vero? Aliquam cum
+            dolorum eveniet modi quasi?
+        </p>
+        <img src="../imgs/fps/gameplays/cs2_gameplay.png" alt="CS2GAMEPLAY">
+    </section>
 </main>
