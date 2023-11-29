02cc50b118c11b5a46204a368ae4328159905074
Ramon253 <jgalazc253@g.educaand.es>
2023-11-29 01:09:00 +0100

    Ejercicio 7 casi listo

--- practicas/practica3/ejercicio7/ejercicio7.php
+++ practicas/practica3/ejercicio7/ejercicio7.php
@@ -1,32 +1,59 @@
 <?php
-date_default_timezone_set("Europe/Madrid");
-getDates($_POST["date"], $_POST["time"]);
-
+/*
+getDates($_POST["date"], $_POST["time"]);*/
+getDates("2020-12-12", "02:01");
 function getDates($date, $time)
 {
-
-    $userBirthday = date_create($date . $time);
+    $weekend = ["Sat" => true, "Sun" => true];
+    $daysOfWeek = [
+        "Sun" => 'Domingo',
+        "Mon" => 'Lunes',
+        "Tue" => 'Martes',
+        "Wed" => 'Miércoles',
+        "Thu" => 'Jueves',
+        "Fri" => 'Viernes',
+        "Sat" => 'Sábado'
+    ];
+    $userBirthday = new DateTime($date . $time);
     $timeUntilChristmas = getNextChristmas();
     $timeUntilEaster = getNextEaster();
+    $season = getSeason();
     $name = $_POST["name"];
-    print "Easter <br>";
-    print "$timeUntilEaster->d dias/$timeUntilEaster->m meses/$timeUntilEaster->y años <br>";
-    print "Christmas <br>";
-    print "$timeUntilChristmas->d dias/$timeUntilChristmas->m meses/$timeUntilChristmas->y años <br>";
-    print date_format($userBirthday, "d/m/Y H:i ") . '<br>';
+
+    print "Bienvenido $name ,";
+    print "estás en $season ";
+    print "Quedan $timeUntilChristmas->days dias para las vacaciones de navidad y ";
+    print "$timeUntilEaster->days dias $timeUntilEaster->h horas para vacaciones de semana santa del año que viene."; //TODO
+
+    print "Tu cumpleaños ";
+    if ($weekend[$userBirthday->format("D")])
+        print "Cae";
+    print " en finde y es el dia ";
+    print   $daysOfWeek[$userBirthday->format("D")] . ", " . $userBirthday->format("d");
 
 }
 
-function getSeason()
+function getSeason($date = new DateTime())
 {
+    $seasons = [
+        "Primavera" => new DateTime("March 20"),
+        "Verano" => new DateTime("June 20"),
+        "Fall" => new DateTime("September 20"),
+        "Invierno" => new DateTime("December 22"),
+    ];
+    $curDate = $date;
 
-
+    foreach ($seasons as $seasonName => $season) {
+        if ($season < $curDate && $curDate < next($seasons))
+            return ($seasonName === "Fall") ? 'Otoño' : $seasonName;
+    }
+    return array_key_last($seasons);
 }
 
 function getNextChristmas(): DateInterval
 {
     $curDate = new DateTime();
-    $christmas = new DateTime("12/ 25");
+    $christmas = new DateTime("December 25");
 
     if ($curDate > $christmas)
         date_modify($christmas, "+1 year");
@@ -35,7 +62,8 @@ function getNextChristmas(): DateInterval
     return date_diff($curDate, $christmas);
 }
 
-function getNextEaster():DateInterval{
+function getNextEaster(): DateInterval
+{
     $curDate = new DateTime();
     $easter = new DateTime("March 24");
 
